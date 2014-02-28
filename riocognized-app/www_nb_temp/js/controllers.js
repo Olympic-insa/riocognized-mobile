0e10ac4b1a9eaa88dad3f0973d2af9a64bdae294
clement <clement@clement-Latitude-E6500>
2014-02-28 12:49:14 +0100

    First display bug => okay Bug => détail d'un athlete

--- riocognized-app/www_nb_temp/js/controllers.js
+++ riocognized-app/www_nb_temp/js/controllers.js
@@ -2,14 +2,15 @@ angular.module('starter.controllers', [])
 
 
 // A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.athletes = PetService.all();
+.controller('PetIndexCtrl', function($scope, AthleteService) {
+    AthleteService.all().success(function(athletes){
+        $scope.athletes=athletes;
+    });
 })
 
 
 // A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+.controller('PetDetailCtrl', function($scope, $stateParams, AthleteService) {
   // "Pets" is a service returning mock data (services.js)
-  $scope.athlete = PetService.get($stateParams.athleteId);
+  $scope.athlete = AthleteService.get($stateParams.athleteId);
 });
--- riocognized-app/www_nb_temp/js/services.js
+++ riocognized-app/www_nb_temp/js/services.js
@@ -1,37 +1,15 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('PetService', function($http) {
-   var athletes;
-  // Might use a resource here that returns a JSON array
-  $http({method: 'GET', url: 'http://olympic-insa.fr.nf:8083/api/athletes'}).
-  success(function(data, status, headers, config) {
-      // this callback will be called asynchronously
-      // when the response is available
-      athletes = data;
-  }).
-  error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-        // Some fake testing data
-    athletes = [
-      { id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-      { id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-      { id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
-      { id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
-    ];
-  });
-
-
+.factory('AthleteService', function($http) {
+  var url = "http://olympic-insa.fr.nf:8083/api/athletes";
+   
   return {
     all: function() {
-      return athletes;
+      return $http.get(url);
     },
     get: function(athleteId) {
-      // Simple index lookup
-      return athletes[athleteId];
+      url = url +"/"+athleteId;
+      return $http.get(url);
     }
   }
 });
